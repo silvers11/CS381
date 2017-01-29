b16434c5eee75ffc87811408f605b5e2bdf4a5f0
Steven Silvers <Steven Silvers>
2017-01-29 14:42:50 -0800

    half done with hw2

--- MiniLogo.silverss.hs
+++ MiniLogo.silverss.hs
@@ -29,11 +29,11 @@ type Var = String
 type Macro = String
 type Prog = [Cmd]
 
-data Mode = Down
-          | Up
+data Mode = Up
+          | Down
    deriving (Eq,Show)
 
-data Expr = Ref Var
+data Expr = Refer Var
           | Lit Num 
           | Add Expr Expr
    deriving (Eq,Show)
@@ -44,5 +44,25 @@ data Cmd = Pen Mode
          | Call Macro [Expr]
   deriving (Eq,Show)
 
+-- 2.) define line macro
+-- lift pen up, move to starting coordinate, set pen down, move to finish coordinate
+
+-- define line (x1,y1,x2,y2) 
+--       Pen Up; Move (x1,y1); Pen Down; Move (x2,y2);
+
+line :: Cmd
+line = Define "line" ["x1", "y1", "x2", "y2"] [Pen Up, Move (Refer "x1") (Refer "y1"), Pen Down, Move (Refer "x2") (Refer "y2")]
 
 
+-- 3.) use line to define nix macro
+-- manipulate line calls with starting reference x,y by adding w to x or h to y
+
+-- Define nix (x,y,w,h) {
+--     line(x,y,x+w,y+h) -- bottom left corner to top right corner
+--     line(x,y+h,x+w,y) --  top right corner to bottom left corner
+-- }
+
+nix :: Cmd
+nix = Define "nix" ["x","y","w","h"] 
+                   [Call "line" [(Refer "x"), (Refer "y"), (Add (Refer "x") (Refer "w")), (Add (Refer "y") (Refer "h"))]
+                  , Call "line" [(Refer "x"), (Add (Refer "y") (Refer "h")), (Add (Refer "x") (Refer "w")), (Refer "y")] ]
