42733ac4def22c540b55c359c030a9e6b185b5cd
Steven Silvers <stevensilvers11@gmail.com>
2017-02-27 14:38:42 -0800

    hw4 progress

--- HW4/HW4/KarelSemantics.hs
+++ HW4/HW4/KarelSemantics.hs
@@ -22,13 +22,23 @@ stmt :: Stmt -> Defs -> World -> Robot -> Result
 stmt Shutdown   _ _ r = Done r
 stmt Move d w (p, c, i) = let newpos = neighbor c p
                           in if isClear newpos w 
-                          	 then OK w (setPos newpos (p, c, i))
-                          	 else Error ("Not clear: " ++ show newpos)
+                             then OK w (setPos newpos (p, c, i))
+                             else Error ("Blocked at: " ++ show newpos)
 stmt PickBeeper _ w r = let p = getPos r
                         in if hasBeeper p w
                               then OK (decBeeper p w) (incBag r)
                               else Error ("No beeper to pick at: " ++ show p)
-                              
+stmt PutBeeper _ w r = let p = getPos r
+                       in if not (isEmpty r)
+                          then OK (incBeeper p w) (decBag r)
+                          else Error "No beeper to put."
+stmt (Turn d) _ w (p, c, i) = OK w (setFacing (cardTurn d c) (p, c, i))
+stmt (If check t f) d w r = if test check w r then stmt t d w r else stmt f d w r
+
+stmt (Block []) d w r = OK w r
+stmt (Block (h:hs)) d w r = case stmt h d w r of
+                            OK wor rob -> stmt (Block hs) d wor rob
+                            otherwise -> otherwise
 stmt _ _ _ _ = undefined
     
 -- | Run a Karel program.
